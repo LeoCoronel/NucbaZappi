ba18118123e1ac623cd97adb028690daa6ce67b9
Leonardo Coronel <leomcoronel99@gmail.com>
2022-09-09 22:32:45 -0300

    Se agregan batidos y tacos a foods.js

--- scripts/foods.js
+++ scripts/foods.js
@@ -1,44 +1,157 @@
-let foods = [
-    {
-        id: 1,
-        nombre: "mozzarella",
-        image: "./img/Products/mozzarella.jpg",
-        ventas: 98,
-        precio: 500
-    },
-    {
-        id: 2,
-        nombre: "fugazzeta",
-        image: "./img/Products/fugazzeta.jpg",
-        ventas: 36,
-        precio: 600
-    },
-    {
-        id: 3,
-        nombre: "especial",
-        image: "./img/Products/especial.jpg",
-        ventas: 85,
-        precio: 550
-    },
-    {
-        id: 4,
-        nombre: "roquefort",
-        image: "./img/Products/roquefort.jpg",
-        ventas: 62,
-        precio: 700
-    },
-    {
-        id: 5,
-        nombre: "calabresa",
-        image: "./img/Products/calabresa.jpg",
-        ventas: 79,
-        precio: 650
-    },
-    {
-        id: 6,
-        nombre: "jamon con huevo",
-        image: "./img/Products/jamonconhuevo.jpg",
-        ventas: 80,
-        precio: 600
-    },
-];
+export let foods = {
+    PIZZAS : [
+        {
+            id: 1,
+            nombre: "mozzarella",
+            image: "./img/Products/mozzarella.jpg",
+            ventas: 98,
+            precio: 500
+        },
+        {
+            id: 2,
+            nombre: "fugazzeta",
+            image: "./img/Products/fugazzeta.jpg",
+            ventas: 36,
+            precio: 600
+        },
+        {
+            id: 3,
+            nombre: "especial",
+            image: "./img/Products/especial.jpg",
+            ventas: 85,
+            precio: 550
+        },
+        {
+            id: 4,
+            nombre: "roquefort",
+            image: "./img/Products/roquefort.jpg",
+            ventas: 62,
+            precio: 700
+        },
+        {
+            id: 5,
+            nombre: "calabresa",
+            image: "./img/Products/calabresa.jpg",
+            ventas: 79,
+            precio: 650
+        },
+        {
+            id: 6,
+            nombre: "jamon con huevo",
+            image: "./img/Products/jamonconhuevo.jpg",
+            ventas: 80,
+            precio: 600
+        },
+    ],
+
+    TACOS : [
+        {
+            id: 1,
+            nombre: "Tacos al pastor",
+            image: "https://www.comedera.com/wp-content/uploads/2017/08/tacos-al-pastor-receta.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 2,
+            nombre: "Tacos de canasta",
+            image: "https://www.comedera.com/wp-content/uploads/2021/03/shutterstock_710462986-tacos-de-canasta-1-500x500.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 3,
+            nombre: "Tacos de carnitas",
+            image: "https://www.recetas360.com/wp-content/uploads/2022/06/como-hacer-tacos-de-carnitas-mexicanos.webp",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 4,
+            nombre: "Tacos de guisado",
+            image: "https://dam.cocinafacil.com.mx/wp-content/uploads/2019/02/tacos-de-guisado2.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 5,
+            nombre: "Tacos de pescado",
+            image: "https://mexico.didiglobal.com/wp-content/uploads/sites/5/2022/03/tacos-de-pescado-980x653.jpg",
+            ventas: 0,
+            precio: 350
+        }
+    ],
+
+    BATIDOS : [
+        {
+            id: 1,
+            nombre: "Batido de arandanos",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-arandanos.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 2,
+            nombre: "Batido de grosella",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-grosella.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 3,
+            nombre: "Batido de cacao y banana",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-cacao-y-platano.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 4,
+            nombre: "Batido de mango",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-mango.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 5,
+            nombre: "Batido de dátiles",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-datiles.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 6,
+            nombre: "Batido de palta y piña",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-aguacate-y-pina.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 7,
+            nombre: "Batido de frutas tropicales",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-frutas-tropicales.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 8,
+            nombre: "Batido de mandarina y mango",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-mandarina-y-mango.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 9,
+            nombre: "Batido de frutilla",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-fresas.jpg",
+            ventas: 0,
+            precio: 350
+        },
+        {
+            id: 10,
+            nombre: "Batido de granada",
+            image: "https://recetinas.com/wp-content/uploads/2022/05/batido-de-granada.jpg",
+            ventas: 0,
+            precio: 350
+        }
+    ]
+};
